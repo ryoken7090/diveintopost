d5cac0ad8174f79139c408650fbf2dcad63fc293
ryoken nakayama <ryoken7090@gmail.com>
2020-07-22 12:06:52 +0900

    半角スペースの調節

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -25,9 +25,9 @@ class AgendasController < ApplicationController
     if current_user.teams.include?(@agenda.team)
       @agenda.destroy
       DestroyAgendaMailer.destroy_agenda_mail(@agenda).deliver
-      redirect_to dashboard_url, notice:'アジェンダが削除されました'
+      redirect_to dashboard_url, notice: 'アジェンダが削除されました'
     else
-      redirect_to dashboard_url, notice:'他チームのアジェンダは削除できません'
+      redirect_to dashboard_url, notice: '他チームのアジェンダは削除できません'
     end
   end
 
