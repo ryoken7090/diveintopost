77f3b1f0f6f5ded84aa9bf24537d7459391130e8
ryoken nakayama <ryoken7090@gmail.com>
2020-07-23 18:20:38 +0900

    チームオーナー以外のteamの編集を制限

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -15,7 +15,11 @@ class TeamsController < ApplicationController
     @team = Team.new
   end
 
-  def edit; end
+  def edit
+    unless current_user == @team.owner
+      redirect_to @team, notice: "チームの編集権限がありません"
+    end
+  end
 
   def create
     @team = Team.new(team_params)
